13566041a4db11f724cb1463f508749af9e3e955
Moses Naworu <mosesnaworu@gmail.com>
2022-10-25 20:47:40 -0500

    code modify

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		String name = "moses";
+		String name = "Moses";
 
 		System.out.println(name);
 
